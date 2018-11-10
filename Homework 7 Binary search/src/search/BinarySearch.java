637a5708a4778fa17217118eb4f3d2e19f496606
Nikolay Yarlychenko <yarlychenkonikolay@gmail.com>
2018-11-10 14:00:19 +0300

    Wrote beta version of BinarySearch(With many cruthces)

--- Homework 7 Binary search/src/search/BinarySearch.java
+++ Homework 7 Binary search/src/search/BinarySearch.java
@@ -9,23 +9,41 @@ import java.util.List;
 public class BinarySearch {
     public static void main(String[] args) {
 
-        int x = Integer.parseInt(args[0]);
-        List<Integer> array = new ArrayList<>();
+        long x = Long.parseLong(args[0]);
+        List<Long> array = new ArrayList<>();
         for (int i = 1; i < args.length; i++) {
-            array.add(Integer.parseInt(args[i]));
+            array.add(Long.parseLong(args[i]));
         }
 
 
         int left = 0, right = array.size() - 1;
+        if (array.size() == 0) {
+            System.out.println(0);
 
-        while (right - left > 1) {
-            int middle = (left + right) / 2;
-            if (array.get(middle) >= x) {
-                left = middle;
-            } else  {
-                right = middle;
+        } else {
+            while (right - left > 2) {
+                int middle = (left + right) / 2;
+                if (array.get(middle) > x) {
+                    left = middle;
+                } else {
+                    right = middle;
+                }
             }
+
+           // System.out.println(left + " " + right);
+
+            boolean check = false;
+            for(int i = left; i <= right; i++) {
+                if(array.get(i) <= x) {
+                    System.out.println(i);
+                    check = true;
+                    break;
+                }
+            }
+            if(!check ) {
+                System.out.println(array.size());
+            }
+
         }
-        System.out.println(left);
     }
 }
