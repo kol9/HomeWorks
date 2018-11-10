98e48ec2699634c3cb9fec9ea78a390add70c792
Nikolay Yarlychenko <yarlychenkonikolay@gmail.com>
2018-11-10 12:49:57 +0300

    Deleted non-useful folders

--- Homework 7 Binary search/src/BinarySearch.java
+++ Homework 7 Binary search/src/BinarySearch.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Nikolay Yarlychenko on 10/11/2018
  */
 public class BinarySearch {
+    public static void main(String[] args) {
 
+        int x = Integer.parseInt(args[0]);
+        List<Integer> array = new ArrayList<>();
+        for (int i = 1; i < args.length; i++) {
+            array.add(Integer.parseInt(args[i]));
+        }
+
+
+        int left = 0, right = array.size() - 1;
+
+        while (right - left > 1) {
+            int middle = (left + right) / 2;
+            if (array.get(middle) >= x) {
+                left = middle;
+            } else  {
+                right = middle;
+            }
+        }
+        System.out.println(left);
+    }
 }
