12c8fad87aa8ed5ff14f40b0700c7e7639b66588
Nikolay Yarlychenko <yarlychenkonikolay@gmail.com>
2018-11-12 23:28:44 +0300

    Wrote beta version of BinarySearch(With many cruthces)

--- Homework 7 Binary search/src/search/BinarySearch.java
+++ Homework 7 Binary search/src/search/BinarySearch.java
@@ -1,49 +1,48 @@
 package search;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
 /**
  * Created by Nikolay Yarlychenko on 10/11/2018
  */
-public class BinarySearch {
-    public static void main(String[] args) {
 
-        long x = Long.parseLong(args[0]);
-        List<Long> array = new ArrayList<>();
+public class BinarySearch {
+
+
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        List<Integer> array = new ArrayList<>();
         for (int i = 1; i < args.length; i++) {
-            array.add(Long.parseLong(args[i]));
+            array.add(Integer.parseInt(args[i]));
         }
 
 
         int left = 0, right = array.size() - 1;
-        if (array.size() == 0) {
-            System.out.println(0);
 
+        while (left <= right) {
+            int middle = (left + right) >>> 1;
+            if (array.get(middle) > x) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        System.out.println(left);
+
+    }
+
+    public int RecursiveBinarySearch(int left, int right) {
+        if (left > right) {
+            return left;
+        }
+        int middle = (left + right) >>> 1;
+        if (array.get(middle) > x) {
+            return RecursiveBinarySearch(left + 1, right);
         } else {
-            while (right - left > 2) {
-                int middle = (left + right) / 2;
-                if (array.get(middle) > x) {
-                    left = middle;
-                } else {
-                    right = middle;
-                }
-            }
-
-           // System.out.println(left + " " + right);
-
-            boolean check = false;
-            for(int i = left; i <= right; i++) {
-                if(array.get(i) <= x) {
-                    System.out.println(i);
-                    check = true;
-                    break;
-                }
-            }
-            if(!check ) {
-                System.out.println(array.size());
-            }
-
+            return RecursiveBinarySearch(left + 1, right);
         }
     }
 }
